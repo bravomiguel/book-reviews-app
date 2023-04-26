73485ab84af0eee9c52eb8179f8e3ea05f1aff09
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-04-26 16:30:56 +0100

    add favicon and title in head

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import Container from '@mui/material/Container';
@@ -13,26 +14,33 @@ import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <header>
-          <Header />
-        </header>
-        <main>
-          <Container
-            maxWidth="md"
-            sx={{
-              pt: '6em',
-              pb: { xs: '4em', sm: '8em' },
-            }}
-          >
-            <Component {...pageProps} />
-          </Container>
-        </main>
-        <footer>
-          <BottomNav />
-        </footer>
-      </CssBaseline>
-    </ThemeProvider>
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <title>Lolo's Reads</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <header>
+            <Header />
+          </header>
+          <main>
+            <Container
+              maxWidth="md"
+              sx={{
+                pt: '6em',
+                pb: { xs: '4em', sm: '8em' },
+              }}
+            >
+              <Component {...pageProps} />
+            </Container>
+          </main>
+          <footer>
+            <BottomNav />
+          </footer>
+        </CssBaseline>
+      </ThemeProvider>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React, {useEffect} from 'react';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
