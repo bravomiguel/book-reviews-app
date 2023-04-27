78430df4f9a6313d13d908f1ee70018b6ac7ce0f
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-04-27 11:19:30 +0100

    Update page UI

--- pages/update/[id].js
+++ pages/update/[id].js
@@ -1,22 +1,31 @@
+import Typography from '@mui/material/Typography';
+import BookForm from '@/components/forms/bookForm';
 import data from '../../dummyData';
 
-export default function Update({id}) {
+export default function Update({ id }) {
+  const idx = data.findIndex((book) => book._id === id);
+  const book = data.at(idx);
   return (
-    <div>Update {id}</div>
-  )
+    <>
+      <Typography variant="h4" component="h3" sx={{ marginBottom: '0.5em' }}>
+        Update Review
+      </Typography>
+      <BookForm book={book} />
+    </>
+  );
 }
 
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
   return {
     props: {
       id: params.id,
-    }
-  }
+    },
+  };
 }
 
 export async function getStaticPaths() {
   const books = data;
-  const paths = books.map((book) => ({params: { id: book._id}}));
+  const paths = books.map((book) => ({ params: { id: book._id } }));
   return {
     paths,
     fallback: true,
