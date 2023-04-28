9e5b3bc7cccb73f199417b1a01155644de011360
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-04-28 15:21:43 +0100

    refactor search books with axios

--- redux/slices/bookSearchSlice.js
+++ redux/slices/bookSearchSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import debounce from 'lodash/debounce';
+import axios from 'axios';
 
 import { GOOGLE_BOOKS_ENDPOINT } from '@/settings';
 
@@ -13,26 +14,20 @@ const initialState = {
 
 const handler = async (searchTerms) => {
   const queryURL = `${GOOGLE_BOOKS_ENDPOINT}/volumes?q=${searchTerms}&key=${process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY}`;
-  try {
-    const response = await fetch(queryURL);
-    console.log(response.status);
-    if (!response.ok) throw response;
-    const rawData = await response.json();
-    let data = [];
-    for (const item of rawData.items) {
-      let result = {
-        id: item.id,
-        title: item.volumeInfo.title,
-        author: item.volumeInfo.authors?.at(0),
-        avatarUrl: item.volumeInfo.imageLinks?.thumbnail,
-      };
-      data = data.concat(result);
-    }
-    return data;
-  } catch (err) {
-    console.log(err.statusText || err.message);
-    return Promise.reject(err.statusText || err.message);
+  const response = await axios(queryURL);
+  if (response.status !== 200) return Promise.reject(response.statusText);
+  const rawData = response.data;
+  let data = [];
+  for (const item of rawData.items) {
+    let result = {
+      id: item.id,
+      title: item.volumeInfo.title,
+      author: item.volumeInfo.authors?.at(0),
+      avatarUrl: item.volumeInfo.imageLinks?.thumbnail,
+    };
+    data = data.concat(result);
   }
+  return data;
 };
 const debouncedHandler = debounce(handler, 200, { leading: true });
 
