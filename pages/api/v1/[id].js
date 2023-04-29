8405cc7665e7d9f25d0a50239f028af781bf71fd
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-04-29 20:27:52 +0100

    add put and delete apis

---
+++ pages/api/v1/[id].js
@@ -0,0 +1,33 @@
+import dbConnect from '@/lib/db';
+import Book from '@/lib/models/book.model';
+
+export default async function handler(req, res) {
+  if (req.method === 'PUT') {
+    const { id } = req.query;
+    const updates = req.body;
+    try {
+      await dbConnect();
+      const result = await Book.updateOne({ _id: id }, updates);
+      if (result.n === 0) return res.status(400);
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else if (req.method === 'DELETE') {
+    const { id } = req.query;
+    try {
+      await dbConnect();
+      const result = await Book.deleteOne({ _id: id });
+      if (result.n === 0) return res.status(404);
+      res.status(204).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res
+      .status(400)
+      .send(
+        `Method ${req.method} not supported for ${new URL(req.url).pathname}`,
+      );
+  }
+}
