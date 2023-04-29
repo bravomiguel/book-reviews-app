85b357ef5585ae494873e43e48f3c0c1d3e3d700
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-04-29 20:08:47 +0100

    add get and post apis

--- pages/api/v1/index.js
+++ pages/api/v1/index.js
@@ -1,6 +1,37 @@
-import dbConnect from "../../server/db";
-import Book from "../../server/models/book.model";
+import dbConnect from '@/lib/db';
+import Book from '@/lib/models/book.model';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    let query = {};
+    const { id } = req.query;
+    if (id) {
+      query._id = id;
+    }
+    try {
+      await dbConnect();
+      const data = await Book.find(query);
+      res.status(200).send(data);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  } else if (req.method === 'POST') {
+    const bookData = req.body;
+    console.log('bookData', bookData);
+    try {
+      await dbConnect();
+      const newBook = new Book(bookData);
+      const result = await newBook.save();
+      res.status(201).send(result);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+    // const book = new Book(bookData);
+  } else {
+    res
+      .status(400)
+      .send(
+        `Method ${req.method} not supported for ${new URL(req.url).pathname}`,
+      );
+  }
 }
