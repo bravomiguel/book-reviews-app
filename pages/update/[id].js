cfcf30e0d7ca6c82b6edb3ad2e0d2d754680f732
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-05-03 10:23:01 +0100

    pre-fetch book in update page

--- pages/update/[id].js
+++ pages/update/[id].js
@@ -1,10 +1,10 @@
 import Typography from '@mui/material/Typography';
 import BookForm from '@/components/forms/bookForm';
-import data from '../../dummyData';
 
-export default function Update({ id }) {
-  const idx = data.findIndex((book) => book._id === id);
-  const book = data.at(idx);
+import dbConnect from '@/lib/db';
+import Book from '@/lib/models/book.model';
+
+export default function Update({ book }) {
   return (
     <>
       <Typography variant="h4" component="h3" sx={{ marginBottom: '0.5em' }}>
@@ -16,16 +16,21 @@ export default function Update({ id }) {
 }
 
 export async function getStaticProps({ params }) {
+  await dbConnect();
+  const book = await Book.findById(params.id);
+  
   return {
     props: {
       id: params.id,
+      book: JSON.parse(JSON.stringify(book)),
     },
   };
 }
 
 export async function getStaticPaths() {
-  const books = data;
-  const paths = books.map((book) => ({ params: { id: book._id } }));
+  await dbConnect();
+  const books = await Book.find({});
+  const paths = books.map((book) => ({params: { id: JSON.parse(JSON.stringify(book._id))}}));
   return {
     paths,
     fallback: true,
