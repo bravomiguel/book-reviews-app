7d128b267acd3c9562df2ad98652edb3792c504c
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-05-22 20:52:05 +0100

    Revert "rebuild api"
    This reverts commit 36a05173a84e727ad6a8c7573ae0d9d93124767d.

---
+++ pages/api/v1/[id].js
@@ -0,0 +1,47 @@
+import dbConnect from '@/lib/db';
+import Book from '@/lib/models/book.model';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const { id } = req.query;
+    console.log(id);
+    try {
+      await dbConnect();
+      const data = await Book.findById(id)
+      if (data === null) {
+        res.status(404).send(data);
+      }
+      res.status(200).send(data);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+  if (req.method === 'PUT') {
+    const { id } = req.query;
+    const updates = req.body;
+    try {
+      await dbConnect();
+      const result = await Book.updateOne({ _id: id }, updates);
+      if (result.n === 0) return res.status(400);
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else if (req.method === 'DELETE') {
+    const { id } = req.query;
+    try {
+      await dbConnect();
+      const result = await Book.deleteOne({ _id: id });
+      if (result.n === 0) return res.status(404);
+      res.status(204).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res
+      .status(400)
+      .send(
+        `Method ${req.method} not supported for this endpoint`,
+      );
+  }
+}
---
+++ pages/api/v1/index.js
@@ -0,0 +1,32 @@
+import dbConnect from '@/lib/db';
+import Book from '@/lib/models/book.model';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      await dbConnect();
+      const data = await Book.find({});
+      res.status(200).send(data);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  } else if (req.method === 'POST') {
+    const bookData = req.body;
+    console.log('bookData', bookData);
+    try {
+      await dbConnect();
+      const newBook = new Book(bookData);
+      const result = await newBook.save();
+      res.status(201).send(result);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+    // const book = new Book(bookData);
+  } else {
+    res
+      .status(400)
+      .send(
+        `Method ${req.method} not supported for this endpoint`,
+      );
+  }
+}
